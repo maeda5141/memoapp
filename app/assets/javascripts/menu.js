6484f82236e6a51357703157e3b770a2fb127cbc
maeda5141 <maedak514@gmail.com>
2019-07-09 01:34:27 +0900

    update_header_function

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -1,16 +1,22 @@
 $(function () {
   $(document).on('turbolinks:load', function() {
     
-    $('.icon').on('mouseover', function () {
+    $('.header_button_user').on('click', function () {
       $('.menu').css('display', 'block');
-    }).on('mouseout', function () {
-      $('.menu').css('display', 'none');
     });
-    $('.menu').on('mouseover', function () {
-      $(this).css('display', 'block');
-    }).on('mouseout', function () {
-      $(this).css('display', 'none');
-    });
+    // .on('mouseout', function () {
+    //   $('.menu').css('display', 'none');
+    // });
+    // $('.menu').on('mouseover', function () {
+    //   $(this).css('display', 'block');
+    // }).on('mouseout', function () {
+    //   $(this).css('display', 'none');
+    // });
+    $(document).on('click', function () {
+      if($(event.target).closest('.header_button_user').length === 0) {
+        $('.menu').css('display', 'none');
+      }
+    })
     $('.select_area #delete').on('click', function () {
       let pop = confirm('このメモを削除しますか？');
     
