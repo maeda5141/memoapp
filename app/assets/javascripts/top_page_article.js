37c991098b81be3bb3d197693de9f45f4fa5f368
maeda5141 <maedak514@gmail.com>
2019-07-23 18:24:03 +0900

    edit_top_page_article.js

--- app/assets/javascripts/top_page_article.js
+++ app/assets/javascripts/top_page_article.js
@@ -1,38 +1,41 @@
 $(function () {
-  $('#search').on('keyup', function () {
-    var input = $(this).val();
-    var page = Number($('.current').text());
-    var url = window.location.href + '/api/articles';
-    $.ajax({
-      type: 'GET',
-      url: url,
-      data: {
-        keyword: input,
-        page: page
-      },
-      dataType: 'json'
-    })
-    .done(function (articles) {
-      $('.article').remove();
-      articles.forEach(function (article) {
-        $('.article_list').append(`
-            <li class="article">
-            <div class="article__title">
-              <a href="/articles/${article.id}" %>
-              ${article.title}
-              </a>
-            </div>
-            <p>${article.userName}さんの投稿</p>
-          </li>
-        `);
-        
-      } );
+  $(document).on('turbolinks:load', function () {
     
+    $('#search').on('keyup', function () {
+      var input = $(this).val();
+      var page = Number($('.current').text());
+      var url = location.origin + '/api/articles';
       
-    })
-    .fail(function () {
-    alert('投稿を取得できませんでした');
-      
+      $.ajax({
+        type: 'GET',
+        url: url,
+        data: {
+          keyword: input,
+          page: page
+        },
+        dataType: 'json'
+      })
+      .done(function (articles) {
+        $('.article').remove();
+        articles.forEach(function (article) {
+          $('.article_list').append(`
+          <li class="article">
+          <div class="article__title">
+          <a href="/articles/${article.id}" %>
+          ${article.title}
+          </a>
+          </div>
+          <p>${article.userName}さんの投稿</p>
+          </li>
+          `);
+          
+        } );
+        
+      })
+      .fail(function () {
+        alert('投稿を取得できませんでした');
+        
+      });
     });
   });
 
