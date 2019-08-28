09929c8f92e0b4a973aa1e57668c973cf6fdae3f
maeda5141 <maedak514@gmail.com>
2019-08-29 00:34:19 +0900

    successfuly_root_path_before_login

--- spec/features/article_spec.rb
+++ spec/features/article_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 feature 'tweet', type: :feature do
+let(:user) { create(:user)}
 
+  scenario 'ログイン前には投稿ボタンが表示されない' do
+  visit root_path
+  expect(page).to have_no_content('\n投稿する')
 
+  end
 end
