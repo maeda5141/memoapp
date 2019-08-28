e223dea6b019db8de10f1f13369cdffa15a293ad
maeda5141 <maedak514@gmail.com>
2019-08-29 00:54:12 +0900

    successfully_login_test

--- spec/features/article_spec.rb
+++ spec/features/article_spec.rb
@@ -3,9 +3,18 @@ require 'rails_helper'
 feature 'tweet', type: :feature do
 let(:user) { create(:user)}
 
-  scenario 'ログイン前には投稿ボタンが表示されない' do
+  scenario 'post article' do
+  #ログイン前には投稿ボタンが現れない
   visit root_path
   expect(page).to have_no_content('\n投稿する')
+
+  #ログイン処理
+  visit new_user_session_path
+  fill_in 'user_email', with: user.email
+  fill_in 'user_password', with: user.password
+  find('input[name="commit"]').click
+  expect(current_path).to eq root_path
+  expect(page).to have_content('投稿する')
 
   end
 end
